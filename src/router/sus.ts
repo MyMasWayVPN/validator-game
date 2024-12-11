0e6da53c3d0cdbd1fdc89b6c3d85a253db7aba97
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-12 05:35:23 +0700

    Update sus.ts

--- src/router/sus.ts
+++ src/router/sus.ts
@@ -1,6 +1,6 @@
 import { endpoint, headers, Result } from '../utils'
 
-export default async function sus1(id: number): Promise<Result> {
+export default async function sus(id: number): Promise<Result> {
   const body = `voucherPricePoint.id=266077&voucherPricePoint.price=13000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=SUPER_SUS&shopLang=id_ID` 
   const response = await fetch(endpoint, {
     method: 'POST',
