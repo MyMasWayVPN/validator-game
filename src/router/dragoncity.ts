a0fa57652d2b136580407dcad8f71cceca9d1ebe
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-12 05:18:03 +0700

    Update dragoncity.ts

--- src/router/dragoncity.ts
+++ src/router/dragoncity.ts
@@ -1,7 +1,7 @@
 import { endpoint, headers, Result } from '../utils'
 
 export default async function pb(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=254206&voucherPricePoint.price=65000.0000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=DRAGON_CITY&shopLang=id_ID`
+  const body = `voucherPricePoint.id=254278&voucherPricePoint.price=479000.0000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=DRAGON_CITY&shopLang=id_ID`
   const response = await fetch(endpoint, {
     method: 'POST',
     headers,
