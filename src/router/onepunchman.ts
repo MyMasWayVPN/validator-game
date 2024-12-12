a19f0c197753bfd408fa489c9c5c8b9fddb9dd9c
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 01:15:06 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,6 +1,6 @@
 import { endpoint, headers, Result } from '../utils'
 
-export default async function onepunchman(id: number, zone: number): Promise<Result> {
+export default async function onepunchman(id: string, zone: number): Promise<Result> {
   const body = `voucherPricePoint.id=308472&voucherPricePoint.price=10890.0000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=${zone}&voucherTypeName=ONE_PUNCH_MAN&shopLang=id_ID`
   const response = await fetch(endpoint, {
     method: 'POST',
