247dfa1cdbac53a02208ca0c4c3e38b679283bd4
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 02:36:31 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -39,27 +39,3 @@ export default async function onepunchman(id: string, zone: string = ''): Promis
         return JSON.stringify({ status: 500, msg: 'Internal Server Error' });
     }
 }
-
-
-export default async function onepunchman(id: string, zone: number): Promise<Result> {
-  const body = `productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=`
-  const response = await fetch("https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store", {
-    method: 'POST',
-    myHeaders,
-    body
-  })
-  const data = await response.json()
-  if (response.data.price.userNameGame) {
-    return {
-      success: true,
-      game: 'One Punch Man',
-      id,
-      name: response.data.price.userNameGame
-    }
-  } else {
-    return {
-      success: false,
-      message: 'Not found'
-    }
-  }
-}
