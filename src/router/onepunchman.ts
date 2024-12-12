697298ce6476ea475fea7531f1525188853d2a3c
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 01:37:12 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,4 +1,4 @@
-import { endapigame, hapigames, Result } from '../utils'
+import { endapigames, hapigames, Result } from '../utils'
 
 export default async function onepunchman(id: string, zone: number): Promise<Result> {
   const body = `productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=`
