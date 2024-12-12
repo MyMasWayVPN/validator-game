0b07673e84a0c0bb2564527a31ca9334aa792ea2
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 01:33:41 +0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 export const endpoint = 'https://order-sg.codashop.com/initPayment.action'
 
+export const endapigames = 'https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store'
+
 export const allowedMethod = ['GET', 'HEAD']
 
 export function getUrl(request: Request): URL {
@@ -12,6 +14,10 @@ export function timeNow(): number {
 
 export const headers = new Headers({
   'Content-Type': 'application/x-www-form-urlencoded'
+})
+
+export const hapigames = new Headers({
+  'Content-Type': 'application/json'
 })
 
 export interface Result {
