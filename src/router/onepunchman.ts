a3fd4534d4c30ef775138ec91af5ed5ab1013b50
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 01:57:13 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,18 +1,32 @@
 import { endapigames, hapigames, Result } from '../utils'
 
 export default async function onepunchman(id: string, zone: number): Promise<Result> {
-  const body = `productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=`
-  const response = await fetch(endpoint, {
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const config: AxiosRequestConfig = {
     method: 'POST',
-    hapigames,
-    body
-  })
-  const data = await response.text()
-  return {
+    url: 'https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store?productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=',
+    headers: { 
+        'User-Agent': 'Apidog/1.0.0 (https://apidog.com)', 
+        'Accept': '*/*', 
+        'Host': 'api.duniagames.co.id', 
+        'Connection': 'keep-alive'
+    }
+};
+
+axios(config)
+    .then((response: AxiosResponse) => {
+        const data = await response.data()
+      return {
     success: true,
     game: 'Mobile Legends: Bang Bang',
     id,
     server: zone,
-    name:data.price.userNameGame
-  }
+    name: response.data.price.userNameGame
+      }
+    })
+    .catch((error: any) => {
+        console.log(error);
+    });
+  
 }
