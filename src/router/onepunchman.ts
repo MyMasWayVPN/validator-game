6710b382cedee83e9c5c290cd871fa6eed281b97
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 02:13:18 +0700

    Add files via upload

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,32 +1,30 @@
-import { endapigames, hapigames, Result } from '../utils'
+import { endpoint, headers, Result } from '../utils'
+
+var myHeaders = new Headers();
+myHeaders.append("User-Agent", "Apidog/1.0.0 (https://apidog.com)");
+myHeaders.append("Accept", "*/*");
+myHeaders.append("Host", "api.duniagames.co.id");
+myHeaders.append("Connection", "keep-alive");
 
 export default async function onepunchman(id: string, zone: number): Promise<Result> {
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-
-const config: AxiosRequestConfig = {
+  const body = `productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=`
+  const response = await fetch("https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store", {
     method: 'POST',
-    url: 'https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store?productId=97&itemId=917&product_ref=REG&product_ref_denom=REG&catalogId=2071&paymentId=5140&gameId=${id}&zoneId=${zone}&campaignUrl=',
-    headers: { 
-        'User-Agent': 'Apidog/1.0.0 (https://apidog.com)', 
-        'Accept': '*/*', 
-        'Host': 'api.duniagames.co.id', 
-        'Connection': 'keep-alive'
+    myHeaders,
+    body
+  })
+  const data = await response.json()
+  if (response.data.price.userNameGame) {
+    return {
+      success: true,
+      game: 'One Punch Man',
+      id,
+      name: response.data.price.userNameGame
     }
-};
-
-axios(config)
-    .then((response: AxiosResponse) => {
-        const data = await response.data()
-      return {
-    success: true,
-    game: 'Mobile Legends: Bang Bang',
-    id,
-    server: zone,
-    name: response.data.price.userNameGame
-      }
-    })
-    .catch((error: any) => {
-        console.log(error);
-    });
-  
+  } else {
+    return {
+      success: false,
+      message: 'Not found'
+    }
+  }
 }
