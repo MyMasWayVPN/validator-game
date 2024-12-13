692884ad5adccd2d4ca11aef573a49ffd33155a7
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 19:18:04 +0700

    Update undawn.ts

--- src/router/undawn.ts
+++ src/router/undawn.ts
@@ -1,7 +1,7 @@
 import { endpoint, headers, Result } from '../utils'
 
 export default async function undawn(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=866394&voucherPricePoint.price=149000.0000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=UNDAWN&shopLang=id_ID`
+  const body = `voucherPricePoint.id=866394&voucherPricePoint.price=149000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=UNDAWN&shopLang=id_ID&voucherTypeId=1&gvtId=1`
   const response = await fetch(endpoint, {
     method: 'POST',
     headers,
