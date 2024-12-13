e9b240f79a66003b4861355026954f333ca4aa81
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 19:29:18 +0700

    Update undawn.ts

--- src/router/undawn.ts
+++ src/router/undawn.ts
@@ -1,17 +1,24 @@
 import { endpoint, headers, Result } from '../utils'
 
 export default async function undawn(id: number): Promise<Result> {
-  const body = `voucherPricePoint.id=866394&voucherPricePoint.price=149000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=UNDAWN&shopLang=id_ID&voucherTypeId=1&gvtId=1`
+  const body = `voucherPricePoint.id=866334&voucherPricePoint.price=15000.0000&voucherPricePoint.variablePrice=0&user.userId=${id}&voucherTypeName=UNDAWN&shopLang=id_ID&voucherTypeId=336&gvtId=478`
   const response = await fetch(endpoint, {
     method: 'POST',
     headers,
     body
   })
   const data = await response.json()
-  return {
-    success: true,
-    game: 'UNDAWN',
-    id,
-    name: data.confirmationFields.roles[0].role
+  if (data.success == true) {
+    return {
+      success: true,
+      game: data.confirmationFields.productName,
+      id,
+      name: data.confirmationFields.roles[0].role
+    }
+  } else {
+    return {
+      success: false,
+      message: 'Not found'
+    }
   }
 }
