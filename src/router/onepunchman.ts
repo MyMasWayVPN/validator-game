a1045b8f3584e65cceabbf069bcb94af5af2f1bf
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 18:15:27 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,35 +1,26 @@
-import { endpoint, headers, Result } from '../utils'
-
-const API_URL = "https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store";
-
-export default async function onepunchman(id: number, zone: number): Promise<Result> {
-  const payload = JSON.stringify({
-            productId: 97,
-            itemId: 918,
-            catalogId: 2072,
-            paymentId: 5141,
-            gameId: id,
-            zoneId: zone,
-            product_ref: "REG",
-            product_ref_denom: "REG"
-        });
-  private async request(url: string, postData: string): Promise<Response> {
-        const headers1 = {
-            'Host': 'api.duniagames.co.id',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
-            'Content-Type': 'application/json'
-        };
-
-  const response = await fetch(API_URL, {
+export default async function hi(id: number): Promise<Result> {
+  const body = `voucherPricePoint.id=308475&voucherPricePoint.price=44000&voucherPricePoint.variablePrice=0&user.userId=${id}&user.zoneId=&voucherTypeName=ONE_PUNCH_MAN&shopLang=id_ID`
+  const response = await fetch(endpoint, {
     method: 'POST',
-    headers1,
-    payload
-  });
-    const data = await response.json();
-    if (!response.ok) {
+    headers,
+    body
+  })
+  const data = await response.json()
+  if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-  return dataata.userNameGame;
-
+else if (data.confirmationFields.username) {
+    return {
+      success: true,
+      game: 'ONE_PUNCH_MAN',
+      id,
+      name: data.confirmationFields.username
+    }
+  }
+  else {
+    return {
+      success: false,
+      message: 'Not found'
+    }
   }
 }
