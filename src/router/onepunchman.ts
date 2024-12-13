6c1ed83920c8790929241a581e4e2f80e2320390
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 17:55:05 +0700

    Update onepunchman.ts

--- src/router/onepunchman.ts
+++ src/router/onepunchman.ts
@@ -1,76 +1,34 @@
 import { endpoint, headers, Result } from '../utils'
 
-const API_CODASHOP = 'https://order-sg.codashop.com/initPayment.action';
+const API_URL = "https://api.duniagames.co.id/api/transaction/v1/top-up/inquiry/store";
 
-function ONE_PUNCH_MAN(id: string, otherid: string = ''): Promise<Result> {
-    const postData = buildBodys(77832, 5500000.0, 0, id, otherid, 'ONE_PUNCH_MAN', '', 'id_ID', '', '');
-    return Request(API_CODASHOP, postData, 'codashop')
-        .then((response: any) => {
-            const parsedResponse = JSON.parse(response);
-
-            if (parsedResponse.RESULT_CODE && parsedResponse.RESULT_CODE === '10001') {
-                return { status: 429, msg: 'Too many attempts, plz wait 5 seconds' };
-            } else {
-                if (parsedResponse.success) {
-                    return {
-                        status: true,
-                        nickname: decodeURIComponent(parsedResponse.confirmationFields.username)
-                    };
-                } else {
-                    return { status: 400, msg: 'Invalid ID or Server' };
-                }
-            }
-        })
-        .catch((error: any) => {
-            return { status: 500, msg: 'Internal Server Error', error };
+export default async function onepunchman(id: number, zone: number): Promise<Result> {
+  const payload = JSON.stringify({
+            productId: 97,
+            itemId: 918,
+            catalogId: 2072,
+            paymentId: 5141,
+            gameId: id,
+            zoneId: zone,
+            product_ref: "REG",
+            product_ref_denom: "REG"
         });
-}
-
-// Helper functions
-function buildBodys(
-    voucherPricePoint.id: number,
-    voucherPricePoint.price: number,
-    voucherPricePoint.variablePrice: number,
-    user.userId: string,
-    user.zoneId: string,
-    voucherTypeName: string,
-    lvtId: string,
-    shopLang: string,
-    dynamicSkuToken: string,
-    pricePointDynamicSkuToken: string,
-    voucherTypeId: string
-): any {
-    return {
-        voucherPricePoint.id,
-        voucherPricePoint.price,
-        voucherPricePoint.variablePrice,
-        user.userId,
-        user.zoneId,
-        voucherTypeName,
-        lvtId,
-        shopLang,
-        dynamicSkuToken,
-        pricePointDynamicSkuToken,
-        voucherTypeId,
-    };
-}
-
-function Request(url: string, body: any, serviceName: string): Promise<Result> {
-    return new Promise((resolve, reject) => {
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    reject(`Error in ${serviceName} service`);
-                }
-                return response.text();
-            })
-            .then((data) => resolve(data))
-            .catch((error) => reject(error));
-    });
+  const headers1 = {
+            'Host': 'api.duniagames.co.id',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
+            'Content-Type': 'application/json'
+        };
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers1,
+    payload
+  })
+  const data = await response.json()
+  return {
+    success: true,
+    game: 'Mobile Legends: Bang Bang',
+    id,
+    server: zone,
+    name:data.data.userNameGame
+  }
 }
